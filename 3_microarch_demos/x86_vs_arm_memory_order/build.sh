0c1156e6a5ae991def115c5d963b4bf6307a9f3c
HarrisonSec <gzxharrison@gmail.com>
2025-07-29 12:15:57 -0700

    Update build.sh

--- 3_microarch_demos/x86_vs_arm_memory_order/build.sh
+++ 3_microarch_demos/x86_vs_arm_memory_order/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[*] Building without fence..."
-gcc -O2 cpu_memory_model_test.c -o cpu_memory_model_test_nofence -lpthread
+gcc -O2 -g cpu_memory_model_test.c -o cpu_memory_model_test_nofence -lpthread
 
 echo "[*] Building with fence..."
-gcc -O2 -DDO_FENCE cpu_memory_model_test.c -o cpu_memory_model_test_fence -lpthread
+gcc -O2 -g -DDO_FENCE cpu_memory_model_test.c -o cpu_memory_model_test_fence -lpthread
